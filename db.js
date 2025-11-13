8a4b83a82b8d1c9183d56d0310cd1960b06063ad
wongtawee <wongtawiaut@gmail.com>
2025-11-13 22:29:59 +0700

    Update DB

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false 
 });
 
 module.exports = pool;
